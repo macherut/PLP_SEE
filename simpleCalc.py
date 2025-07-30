a2629bcbab7e36d222a65dc689288b73c2974875
macherut <macheru@live.com>
2025-07-30 09:45:49 +0300

    Python Assignment

---
+++ simpleCalc.py
@@ -0,0 +1,13 @@
+numberOne = float(input("Enter the first number"))
+
+numberTwo = float(input("Enter the second number"))
+
+multiplication = numberOne * numberTwo
+addition = numberOne + numberTwo
+quotient = numberOne / numberTwo
+subtraction = numberOne - numberTwo
+
+print(f"Multiplication: {multiplication}")
+print(f"Addition: {addition}")
+print(f"Quotient: {quotient}")
+print(f"Subtraction: {subtraction}")
